052bef6747eb058d6a4289413c639695eed5d4ea
TeaMitic <tea.mitic20@gmail.com>
2022-05-10 00:04:48 +0200

    addOne, addMany func - not tested

--- Projekat1/MicroService-DB/controllers/songsController.js
+++ Projekat1/MicroService-DB/controllers/songsController.js
@@ -1,5 +1,5 @@
 const mongoose= require('mongoose')
-const {Song} = require('../models/songModel')
+const Song = require('../models/songModel')
 
 //functions
 
@@ -14,7 +14,7 @@ const AddOne = async (req,res)=>{
             danceability: req.body.danceability,
             loudnesIndB: req.body.loudnesIndB,
             liveness: req.body.liveness,
-            valence: req.body.artvalenceistName,
+            valence: req.body.valence,
             lenght: req.body.lenght,
             acousticness: req.body.acousticness,
             speechiness: req.body.speechiness,
@@ -31,14 +31,43 @@ const AddOne = async (req,res)=>{
         res.status(500).send(error.message)
     }
 }
-//ne treba preko body i req
+
 const AddMany = async (songs)=>{
-    try {
-        songs.forEach(element => {
+    await Song.insertMany(songs).then(()=> {
+        let sendInfo = "Songs successfully added"
+        res.status(200).send(sendInfo)
+    })
+    .catch(function (err) {
+        response.status(500).send(err.message);
+    });
+    // try{
+        // songs.forEach(element => {
+        //     const song = await Song.create({
+        //         trackName: element.trackName,
+        //         artistName: element.artistName,
+        //         genre: element.genre,
+        //         beatsPerMinute: element.beatsPerMinute,
+        //         energy: element.energy,
+        //         danceability: element.danceability,
+        //         loudnesIndB: element.loudnesIndB,
+        //         liveness: element.liveness,
+        //         valence: element.artvalenceistName,
+        //         lenght: element.lenght,
+        //         acousticness: element.acousticness,
+        //         speechiness: element.speechiness,
+        //         popularity: element.popularity
+        //     })
             
-        });
+        // });
+        // let sendInfo = "Songs successfully added"
+        // res.status(200).send(sendInfo)
         
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
+//     } catch (error) {
+//         res.status(500).send(error.message)
+//     }
+}
+
+module.exports ={
+    AddOne,
+    AddMany
 }
--- Projekat1/MicroService-DB/routes/songsRoutes.js
+++ Projekat1/MicroService-DB/routes/songsRoutes.js
@@ -2,8 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const{
+    AddOne,
+    AddMany
 
 } = require('../controllers/songsController')
 
+router.post('/addOne', AddOne)
+router.post('/addMany', AddMany)
 
 module.exports = router
