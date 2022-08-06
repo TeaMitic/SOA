f8b672a4c6f02a31bd3c8039ab026996629b871e
Didi211 <miticd99@gmail.com>
2022-08-07 00:34:23 +0200

    Prezentacija projekata
    Prva dva projekta gotova

--- Projekat1/MicroService-Gateway/Controllers/SongController.cs
+++ Projekat1/MicroService-Gateway/Controllers/SongController.cs
@@ -198,51 +198,7 @@ public class SongController : ControllerBase
         }
     }
 
-    /// <summary>
-    /// Generate data from csv file to mock iot sensor for agriculture data
-    /// </summary>
-    /// <param name="sleepSeconds">Time gap between two sensor readings</param>
-    /// <returns></returns>
-    /// <response code="200">Informs that data is being generated.</response>
-    /// <response code="500">Informs that server error occured during generating the data.</response>
-    [HttpPost]
-    [Route("GenerateData/{sleepSeconds}")]
-    public async Task<IActionResult> GenerateData([FromRoute] int sleepSeconds)
-    {
-        try
-        {  
-           
-            CsvHelperWrapper csvWrapper = new CsvHelperWrapper("/home/datasets/Agriculture/Crop_recommendation.csv");
-            int numOfItemsLoaded = 0;
-            Agriculture? crop;
-            using ( var mqttClient = new MQTTService()) 
-            {
-                while (!csvWrapper.Eof)
-                {
-                    crop =  csvWrapper.ReadMoreRecords<Agriculture?,AgricultureClassMap>(numOfItemsLoaded,1).FirstOrDefault();
-                    if (crop == null) 
-                    {
-                        break;
-                    }
-                    numOfItemsLoaded++;
-
-                    //sending via MQTT 
-                    Console.WriteLine(JsonConvert.SerializeObject(crop));
-                    //simulating the time gap in sensoring the data 
-                    await mqttClient.PublishEvent("analytics/agriculture",crop); 
-                    Thread.Sleep(sleepSeconds*1000); 
-
-                }
-            } 
-            
-            return StatusCode(200,"Generating data finished.");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex);
-            return StatusCode(500,ex);
-        }
-    }
+   
     
     /// <summary>
     /// Edits one song in db.
