6858727223f9477f892a4cccc5fbe963cfe5a3f4
TeaMitic <tea.mitic20@gmail.com>
2022-08-15 23:28:51 +0200

    grpc radi na microservice-notif

--- Projekat1/MicroService-Notif/app.js
+++ Projekat1/MicroService-Notif/app.js
@@ -1,16 +1,19 @@
-const grpc = require("grpc");
+const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 const packageDef = protoLoader.loadSync("notification.proto", {});
 const grpcObject = grpc.loadPackageDefinition(packageDef)
 const notification = grpcObject.notification;
 
 const server = new grpc.Server();
-server.bind("0.0.0.0:8085", grpc.ServerCredentials.createInsecure())
 server.addService(notification.Notification.service,
     {
         "sendNotif": sendNotif
     });
-server.start();
+server.bindAsync("0.0.0.0:8085", grpc.ServerCredentials.createInsecure(), () => {
+    server.start();
+    console.log('grpc server started')
+
+})
 function sendNotif(call, callback){
     console.log(call)
 }
