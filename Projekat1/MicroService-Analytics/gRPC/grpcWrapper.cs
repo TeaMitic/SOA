a82614c4048becdd29ed7e23c788110685fa6d07
Didi211 <miticd99@gmail.com>
2022-08-21 16:46:52 +0200

    indentation in grpc wrapper

--- Projekat1/MicroService-Analytics/gRPC/grpcWrapper.cs
+++ Projekat1/MicroService-Analytics/gRPC/grpcWrapper.cs
@@ -8,13 +8,13 @@ namespace MicroService_Analytics.gRPC
         private static GrpcClient.Notification.NotificationClient? _grpcClient = null;
 
         private static GrpcChannel configureGRPC(){ 
-        // grpc deo
-        var httpHandler = new HttpClientHandler();
-        //Return true to allow certificates that are untrusted/invalid
-        httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
-        var channel = GrpcChannel.ForAddress("http://service-notif:8085",new GrpcChannelOptions {HttpHandler = httpHandler});
-        // CancellationToken cancellationToken = default(CancellationToken)
-        return channel;
+            // grpc deo
+            var httpHandler = new HttpClientHandler();
+            //Return true to allow certificates that are untrusted/invalid
+            httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var channel = GrpcChannel.ForAddress("http://service-notif:8085",new GrpcChannelOptions {HttpHandler = httpHandler});
+            // CancellationToken cancellationToken = default(CancellationToken)
+            return channel;
     }
 
         public static GrpcClient.Notification.NotificationClient GRPCClient {
